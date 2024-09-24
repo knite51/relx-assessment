77a31916ee79a60b11553d4dc4eefddacb51ff31
Ayotunde <knite51@gmail.com>
2024-09-24 08:47:27 +0100

    remove commented codes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,9 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
-import { ApiKeyInterceptor } from './interceptors/api-key.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom([HttpClientModule]),
-    // { provide: HTTP_INTERCEPTORS, useClass: ApiKeyInterceptor, multi: true },
-  ],
+  providers: [provideRouter(routes), importProvidersFrom([HttpClientModule])],
 };
